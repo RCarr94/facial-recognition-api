274d3256553fc0251f1c6c3e2328983b775870ed
Ryan Carr <ryan.g.carr@hotmail.com>
2023-09-30 13:22:58 -0400

    created gitignore to ignore node_modules

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const database = {
       id: '123',
       name: 'John',
       email: 'john@gmail.com',
-      password: 'cookies',
       entries: 0,
       joined: new Date()
     },
@@ -19,10 +18,16 @@ const database = {
       id: '124',
       name: 'Sally',
       email: 'sally@gmail.com',
-      password: 'bananas',
       entries: 0,
       joined: new Date()
     },
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
@@ -53,14 +58,37 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length-1]);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+// need to clean up and make sep function to improve DRY!!
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-
-
-//  THE PLAN
-// res = this is working
-// /signin --> POST = success/fail
-// /register --> POST = user
-// /profile/:userId --> GET = user
-// /image --> PUT --> user
