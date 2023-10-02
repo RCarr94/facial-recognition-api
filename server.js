f6b85b45af9e569c616e3a52a3e33687924f83d3
Ryan Carr <ryan.g.carr@hotmail.com>
2023-10-02 13:11:30 -0400

    added controllers folder, cleaned up server.js file

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
+const register = require('./controllers/register');
+const signIn = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
   client: 'pg',
   connection : {
@@ -16,7 +21,6 @@ const db = knex({
 });
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -54,80 +58,11 @@ app.use(cors());
 //   res.send(database.users);
 // });  
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-    if (isValid) {
-      return db.select('*').from('users')
-        .where('email', '=', req.body.email)
-        .then(user => {
-          res.json(user[0]);
-        })
-        .catch(err => res.status(400).json('unable to get user'))
-    } else {
-      res.status(400).json('wrong credentials')
-    }
-  })
-  .catch(err => res.status(400).json('wrong credentials'))
-});
-
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-
-    .catch(err => res.status(400).json('unable to register'))
-  
-});
-
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-  .then(user => {
-    if (user.length) {
-      res.json(user[0]);
-    } else {
-      res.status(400).json('error getting user');
-    }
-  })
-  .catch(err => res.status(400).json('Not found'))
-});
-
+app.post('/signin', signIn.handleSignIn(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfileGet(db));
 // need to clean up and make sep function to improve DRY!!
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-});
+app.put('/image', image.handleImage(db));
 
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
