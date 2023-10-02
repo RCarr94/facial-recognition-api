474cfff4a9397937fb68173215ddf4e85e82d9a6
Ryan Carr <ryan.g.carr@hotmail.com>
2023-10-02 11:43:59 -0400

    updated signin, register, profile  & image routes to live db

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-  console.log(data);
-})
+// db.select('*').from('users').then(data => {
+//   console.log(data);
+// })
 
 const app = express();
 
@@ -57,60 +57,88 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
-  res.json('signing');
+  db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if (isValid) {
+      return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+    } else {
+      res.status(400).json('wrong credentials')
+    }
+  })
+  .catch(err => res.status(400).json('wrong credentials'))
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-  .returning('*')
-  .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  })
-    .then(user => {
-      res.json(user[0]);
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
+
     .catch(err => res.status(400).json('unable to register'))
   
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('error getting user');
     }
   })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  .catch(err => res.status(400).json('Not found'))
 });
 
 // need to clean up and make sep function to improve DRY!!
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
   })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  .catch(err => res.status(400).json('unable to get entries'))
 });
+
+// .then(entries => {
+//     // If you are using knex.js version 1.0.0 or higher this now 
+//     // returns an array of objects. Therefore, the code goes from:
+//     // entries[0] --> this used to return the entries
+//     // TO
+//     // entries[0].entries --> this now returns the entries
+//     res.json(entries[0].entries);
+//   })
 
   // bcrypt.hash(password, null, null, function (err, hash) {
   //   console.log(hash);
